41222ac6fc6a548ff7889e2e074e036becd5c49c
Pablo Cervilla miguel <pcervill@c3r13s6.42madrid.com>
2022-03-25 11:36:10 +0100

    42cursus

---
+++ libft/main_to.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_to.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 14:19:29 by pcervill          #+#    #+#             */
+/*   Updated: 2022/03/24 14:20:36 by pcervill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+
+int	main(void)
+{
+	int	c;
+	int	c1;
+
+	c = 'a';
+	c1 = 'a';
+	printf("Funcion original: %c\n", toupper(c));
+	printf("Mi	 funcion: %c\n", ft_toupper(c1));
+	printf("Funcion original: %c\n", tolower(c));
+	printf("Mi	 funcion: %c\n", ft_tolower(c1));
+}
