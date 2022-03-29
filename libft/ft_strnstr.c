7f54455e153270b4b0d34a0f16c8101065bad6a7
CERVIII <pablo.cervilla117@gmail.com>
2022-03-29 22:35:37 +0000

    casa29/03

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pcervill <pcervill@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 21:21:18 by pcervill          #+#    #+#             */
-/*   Updated: 2022/03/29 22:04:49 by pcervill         ###   ########.fr       */
+/*   Updated: 2022/03/29 22:29:25 by pcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return (0);
 	contn = ft_strlen(needle);
 	cont = 0;
-	while (contn <= len-- && *haystack + cont != '\0')
+	while (contn <= len && *haystack + cont != '\0')
 	{
 		if (!(ft_strncmp(haystack + cont, needle, contn)))
 			return ((char *)haystack + cont);
 		cont++;
+		len--;
 	}
 	return (0);
 }
